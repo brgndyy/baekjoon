71e91443f943548170d6d724262c0ad7d1a5aba6
brgndy <brgndy96@gmail.com>
2023-04-30 15:41:30 +0900

    ㅡㄴㅎ

---
+++ 브론즈/10812번 브론즈2.js
@@ -0,0 +1,13 @@
+const input = `10 5
+1 6 4
+3 9 8
+2 10 5
+1 3 3
+2 6 2`.split("\n");
+
+const N = Number(input[0].split(" ")[0]);
+const M = Number(input[0].split(" ")[1]);
+
+// 1 2 3 4 5 6 7 8 9 10
+// 4 5 6 7 8 9 10 1 2 3
+//
