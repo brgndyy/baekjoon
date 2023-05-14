bb2a634f45a76fef246ac5531c6ba53a929b0a1b
brgndy <brgndy96@gmail.com>
2023-05-14 20:54:35 +0900

    Msg

--- debug.js
+++ debug.js
@@ -1,21 +1,26 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const N = Number(input[0].split(" ")[0]);
-const M = Number(input[0].split(" ")[1]);
+let N = Number(input.shift());
+let canvas = Array.from({ length: 100 }, (_, index) => Array(100).fill(0));
+let wholeWidHei = 100 * N;
+let count = 0;
 
-let balls = Array.from({ length: N }, (_, index) => index + 1);
+for (let i = 0; i < input.length; i++) {
+  // 가로축
+  let xIndex = Number(input[i].split(" ")[0]); // 3
 
-for (let i = 1; i <= M; i++) {
-  let startNum = Number(input[i].split(" ")[0]); // 1
-  let endNum = Number(input[i].split(" ")[1]); // 6
-  let midNum = Number(input[i].split(" ")[2]); // 4
+  // 세로축
+  let yIndex = 99 - Number(input[i].split(" ")[1]); // 92
 
-  let sliceArr = balls
-    .slice(midNum - 1, endNum)
-    .concat(balls.slice(startNum - 1, midNum - 1));
-
-  balls.splice(startNum - 1, endNum - startNum + 1, ...sliceArr);
+  for (let k = yIndex; k > yIndex - 10; k--) {
+    for (let j = xIndex; j < xIndex + 10; j++) {
+      canvas[k][j]++;
+      if (canvas[k][j] > 1) {
+        count++;
+      }
+    }
+  }
 }
 
-console.log(balls.join(" "));
+console.log(wholeWidHei - count);
