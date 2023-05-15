83b1c1fd082c661f78c975fa7da268b8575d4f82
brgndy <brgndy96@gmail.com>
2023-05-15 11:46:50 +0900

    msg

--- 실버/가로수  실4.js
+++ 실버/가로수  실4.js
@@ -1,12 +1,29 @@
-const input = `5
-1
-3
-7
-13
-17`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let N = Number(input.shift());
 
 let sortedInput = input.map((str) => Number(str)).sort((a, b) => a - b);
+let minusArr = [];
+let gcdArr = [];
+let count = 0;
 
-console.log(sortedInput);
+for (let i = 0; i < input.length - 1; i++) {
+  minusArr.push(Math.abs(input[i] - input[i + 1]));
+}
+
+function getGcd(a, b) {
+  return b % a === 0 ? a : getGcd(b, b % a);
+}
+
+for (let j = 0; j < minusArr.length - 1; j++) {
+  gcdArr.push(getGcd(minusArr[j], minusArr[j + 1]));
+}
+
+let gcdNum = Math.min(...gcdArr);
+
+for (let i = 0; i < minusArr.length; i++) {
+  count += minusArr[i] / gcdNum - 1;
+}
+
+console.log(count);
