df4b38404e39f0acf983e79c8406925996cb6e08
brgndy <brgndy96@gmail.com>
2023-05-15 15:19:42 +0900

    msg

--- 실버/큐 2 실4.js
+++ 실버/큐 2 실4.js
@@ -2,42 +2,40 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let N = Number(input[0]);
+let queue = [];
 let answer = [];
-let arr = [];
 
 for (let i = 1; i <= N; i++) {
-  let splitInput = input[i].split(" ")[0];
+  let splitWord = input[i].split(" ");
 
-  if (splitInput === "push") {
-    let num = Number(input[i].split(" ")[1]);
-    arr.push(num);
-  } else if (splitInput === "front") {
-    if (arr.length === 0) {
+  if (splitWord[0] === "push") {
+    queue.push(Number(splitWord[1]));
+  } else if (splitWord[0] === "front") {
+    if (queue.length === 0) {
       answer.push(-1);
     } else {
-      answer.push(arr[0]);
+      answer.push(queue[0]);
     }
-  } else if (splitInput === "back") {
-    if (arr.length === 0) {
+  } else if (splitWord[0] === "back") {
+    if (queue.length === 0) {
       answer.push(-1);
     } else {
-      answer.push(arr[arr.length - 1]);
+      answer.push(queue[queue.length - 1]);
     }
-  } else if (splitInput === "size") {
-    answer.push(arr.length);
-  } else if (splitInput === "pop") {
-    if (arr.length === 0) {
-      answer.push(-1);
-    } else {
-      answer.push(arr.shift());
-    }
-  } else if (splitInput === "empty") {
-    if (arr.length === 0) {
+  } else if (splitWord[0] === "size") {
+    answer.push(queue.length);
+  } else if (splitWord[0] === "empty") {
+    if (queue.length === 0) {
       answer.push(1);
     } else {
       answer.push(0);
     }
+  } else if (splitWord[0] === "pop") {
+    if (queue.length === 0) {
+      answer.push(-1);
+    } else {
+      answer.push(queue.shift());
+    }
   }
 }
-
 console.log(answer.join("\n"));
