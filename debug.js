c44c5f1a7bb6292ae53fca66039db4ac668889e1
brgndy <brgndy96@gmail.com>
2023-05-21 14:09:29 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,35 +1,27 @@
-let input = `5
-100
-6
-8
-10
-12`.split("\n");
+const input = `7
+ENTER
+pjshwa
+pjshwa
+chogahui05
+ENTER
+pjshwa
+chansol`.split("\n");
 
-let T = Number(input.shift());
+let N = Number(input.shift());
 
-let inputNumArr = input.map((str) => Number(str));
-let answer = [];
+let stack = [];
+let count = 0;
 
-let maxNum = Math.max(...inputNumArr);
-
-let numArr = Array(maxNum).fill(true).fill(false, 0, 2);
-
-for (let i = 2; i <= numArr.length; i++) {
-  for (let j = i * i; j <= numArr.length; j += i) {
-    numArr[j] = false;
+for (let i = 0; i < N; i++) {
+  if (input[i] === "ENTER") {
+    stack.push(input[i + 1]);
+    count++;
+    i++;
+  } else if (stack.filter((mem) => mem === input[i]).length) {
+    continue;
+  } else {
+    count++;
   }
 }
 
-for (let i = 0; i < inputNumArr.length; i++) {
-  let count = 0;
-
-  for (let j = 2; j <= inputNumArr[i] / 2; j++) {
-    if (numArr[j] && numArr[inputNumArr[i] - j]) {
-      count++;
-    }
-  }
-
-  answer.push(count);
-}
-
-console.log(answer);
+console.log(count);
--- 실버/인사성 밝은 곰곰이 실4.js
+++ 실버/인사성 밝은 곰곰이 실4.js
@@ -1,12 +1,27 @@
 const input = `7
 ENTER
 pjshwa
-chansol
+pjshwa
 chogahui05
 ENTER
 pjshwa
 chansol`.split("\n");
 
 let N = Number(input.shift());
+
+let stack = [];
+let count = 0;
+
+for (let i = 0; i < N; i++) {
+  if (input[i] === "ENTER") {
+    stack.push(input[i + 1]);
+    count++;
+    i++;
+  } else if (stack[i - 1] === input[i]) {
+    continue;
+  } else {
+    count++;
+  }
+}
 
 console.log(count);
