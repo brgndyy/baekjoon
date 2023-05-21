4d27f674b8d2a8a2924f36badedcbc2f0bb9573a
brgndy <brgndy96@gmail.com>
2023-05-21 14:19:51 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,27 +1,8 @@
-const input = `7
+let input = `3
 ENTER
-pjshwa
-pjshwa
-chogahui05
-ENTER
-pjshwa
-chansol`.split("\n");
+lms0806
+lms0806`.split("\n");
 
 let N = Number(input.shift());
-
-let stack = [];
-let count = 0;
-
-for (let i = 0; i < N; i++) {
-  if (input[i] === "ENTER") {
-    stack.push(input[i + 1]);
-    count++;
-    i++;
-  } else if (stack.filter((mem) => mem === input[i]).length) {
-    continue;
-  } else {
-    count++;
-  }
-}
 
 console.log(count);
