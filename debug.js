1455eee5978f9ea4c3f960775ee202091e10bcae
brgndy <brgndy96@gmail.com>
2023-05-22 12:01:01 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,17 +1,10 @@
-const input = `12
-bnb2011 chansol
-chansol chogahui05
-chogahui05 jthis
-jthis ChongChong
-jthis jyheo98
-jyheo98 lms0806
-chansol pichulia
-ChongChong pjshwa
-pjshwa r4pidstart
-r4pidstart swoon
-swoon tony9402
-tony9402 bnb2011`.split("\n");
-
-let N = Number(input.shift());
-
-console.log(dancer.size);
+const input = [
+  "LONG",
+  "FLOAT",
+  "SHORT",
+  "BOOL",
+  "SHORT",
+  "INT",
+  "LONG",
+  "BOOL",
+];
