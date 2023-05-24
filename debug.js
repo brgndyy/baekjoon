b25b039544f1220a57cd76fe2f633e7423f13bd5
brgndy <brgndy96@gmail.com>
2023-05-24 10:21:37 +0900

    ㅡsg

--- debug.js
+++ debug.js
@@ -1,10 +1,30 @@
-const input = [
-  "LONG",
-  "FLOAT",
-  "SHORT",
-  "BOOL",
-  "SHORT",
-  "INT",
-  "LONG",
-  "BOOL",
-];
+const input = `5
+55 185
+58 183
+88 186
+60 175
+46 155`.split("\n");
+
+let N = Number(input.shift());
+let answer = [];
+
+let wh = input.map((wh) => [
+  Number(wh.split(" ")[0]),
+  Number(wh.split(" ")[1]),
+]);
+
+for (let i = 0; i < wh.length; i++) {
+  let count = N;
+
+  for (let j = 0; j < wh.length; j++) {
+    if (i !== j) {
+      if (wh[i][0] > wh[j][0] || wh[i][1] > wh[j][1]) {
+        count--;
+      }
+    }
+  }
+
+  answer.push(count);
+}
+
+console.log(answer);
--- 실버/덩치 실5.js
+++ 실버/덩치 실5.js
@@ -6,10 +6,3 @@ const input = `5
 46 155`.split("\n");
 
 let N = Number(input.shift());
-
-let wh = input.map((wh) => [
-  Number(wh.split(" ")[0]),
-  Number(wh.split(" ")[1]),
-]);
-
-console.log(wh);
