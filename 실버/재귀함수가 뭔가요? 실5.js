eb483729ab458cc6f666512c5dd300677283227e
brgndy <brgndy96@gmail.com>
2023-05-24 17:06:59 +0900

    ㅡsg

--- 실버/재귀함수가 뭔가요? 실5.js
+++ 실버/재귀함수가 뭔가요? 실5.js
@@ -1,4 +1,5 @@
-let input = `4`;
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
 
 let N = Number(input);
 
@@ -10,9 +11,9 @@ let answer = [];
 
 while (count <= N) {
   if (count === N) {
-    answer.push(underLine.repeat(count) + `"재귀함수가 뭔가요?"`);
+    answer.push(underLine.repeat(count) + "재귀함수가 뭔가요?");
     answer.push(
-      underLine.repeat(count) + `"재귀함수는 자기 자신을 호출하는 함수라네"`
+      underLine.repeat(count) + "재귀함수는 자기 자신을 호출하는 함수라네"
     );
     answer.push(underLine.repeat(count) + "라고 답변하였지.");
     while (count > 0) {
@@ -23,10 +24,10 @@ while (count <= N) {
     break;
   }
 
-  answer.push(underLine.repeat(count) + `"재귀함수가 뭔가요?"`);
+  answer.push(underLine.repeat(count) + "재귀함수가 뭔가요?");
   answer.push(
     underLine.repeat(count) +
-      `"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.`
+      "잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어."
   );
   answer.push(
     underLine.repeat(count) +
@@ -34,7 +35,7 @@ while (count <= N) {
   );
   answer.push(
     underLine.repeat(count) +
-      `그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."`
+      "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."
   );
 
   count++;
