77134a424ec851521340c08e82b45218d6039832
brgndy <brgndy96@gmail.com>
2023-05-25 13:05:55 +0900

    msg

--- 실버/에라토스테네스의 체 실4.js
+++ 실버/에라토스테네스의 체 실4.js
@@ -1,4 +1,4 @@
-const input = `10 7`;
+const input = `15 12`;
 
 let [N, K] = [Number(input.split(" ")[0]), Number(input.split(" ")[1])];
 
@@ -9,27 +9,22 @@ let arr = Array(N + 1)
 let count = 0;
 let answer = 0;
 
-for (let i = 2; i <= N; i++) {
-  if (arr[i] === true) {
-    arr[i] = false;
-    count++;
-  }
+for (let i = 2; i <= Math.sqrt(N); i++) {
+  count++;
 
   if (count === K) {
     answer = i;
     break;
   }
-
-  for (let j = i * i; j <= N; j += i) {
-    if (j % arr[i] === 0) {
-      if (arr[j] === true) {
+  if (arr[i]) {
+    for (let j = i * i; j <= N; j += i) {
+      if (j % arr[i] === 0) {
         arr[j] = false;
         count++;
-      }
-
-      if (count === K) {
-        answer = j;
-        break;
+        if (count === K) {
+          answer = j;
+          break;
+        }
       }
     }
   }
