210dd3bf9896c5d5137ffdf0db5f0d53c04b77f4
brgndy <brgndy96@gmail.com>
2023-05-25 12:29:04 +0900

    Msg

--- debug.js
+++ debug.js
@@ -1,51 +1,38 @@
-let input = `4`;
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-let N = Number(input);
-
-let underLine = "____";
-
-let count = 1;
+let [N, M] = input
+  .shift()
+  .split(" ")
+  .map((str) => Number(str));
 
 let answer = [];
 
-while (count <= N) {
-  if (count === N) {
-    answer.push(underLine.repeat(count) + `"재귀함수가 뭔가요?"`);
-    answer.push(
-      underLine.repeat(count) + `"재귀함수는 자기 자신을 호출하는 함수라네"`
-    );
-    answer.push(underLine.repeat(count) + "라고 답변하였지.");
-    while (count > 0) {
-      count--;
-      answer.push(underLine.repeat(count) + "라고 답변하였지.");
-    }
+let numArr = [];
 
-    break;
-  }
+for (let i = 0; i < N; i++) {
+  let arr = input
+    .shift()
+    .split(" ")
+    .map((str) => Number(str));
 
-  answer.push(underLine.repeat(count) + `"재귀함수가 뭔가요?"`);
-  answer.push(
-    underLine.repeat(count) +
-      `"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.`
-  );
-  answer.push(
-    underLine.repeat(count) +
-      "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지."
-  );
-  answer.push(
-    underLine.repeat(count) +
-      `그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."`
-  );
-
-  count++;
+  numArr.push(arr);
 }
 
-console.log(
-  `어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.
-"재귀함수가 뭔가요?"
-"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.
-마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.
-그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."` +
-    "\n" +
-    answer.join("\n")
-);
+let K = Number(input.shift());
+
+for (let t = 0; t < K; t++) {
+  // 1 1 2 3
+  let [i, j, x, y] = input[t].split(" ").map((str) => Number(str));
+  let sum = 0;
+
+  for (let k = j - 1; k <= y - 1; k++) {
+    for (let z = i - 1; z <= x - 1; z++) {
+      sum += numArr[z][k];
+    }
+  }
+
+  answer.push(sum);
+}
+
+console.log(answer.join("\n"));
--- 실버/2차원 배열의 합 실5.js
+++ 실버/2차원 배열의 합 실5.js
@@ -1,18 +1,38 @@
-const input = `2 3
-1 2 4
-8 16 32
-3
-1 1 2 3
-1 2 1 2
-1 3 2 3`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let [N, M] = input
   .shift()
   .split(" ")
   .map((str) => Number(str));
 
+let answer = [];
+
 let numArr = [];
 
-for (let i = 0; i < N; i++) {}
+for (let i = 0; i < N; i++) {
+  let arr = input
+    .shift()
+    .split(" ")
+    .map((str) => Number(str));
 
-console.log(N, M);
+  numArr.push(arr);
+}
+
+let K = Number(input.shift());
+
+for (let t = 0; t < K; t++) {
+  // 1 1 2 3
+  let [i, j, x, y] = input[t].split(" ").map((str) => Number(str));
+  let sum = 0;
+
+  for (let k = j - 1; k <= y - 1; k++) {
+    for (let z = i - 1; z <= x - 1; z++) {
+      sum += numArr[z][k];
+    }
+  }
+
+  answer.push(sum);
+}
+
+console.log(answer.join("\n"));
