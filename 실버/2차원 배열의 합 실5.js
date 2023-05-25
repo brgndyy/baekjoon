42401c2f1a97578280f673e235a994f6073916a6
brgndy <brgndy96@gmail.com>
2023-05-25 12:03:52 +0900

    msg

---
+++ 실버/2차원 배열의 합 실5.js
@@ -0,0 +1,18 @@
+const input = `2 3
+1 2 4
+8 16 32
+3
+1 1 2 3
+1 2 1 2
+1 3 2 3`.split("\n");
+
+let [N, M] = input
+  .shift()
+  .split(" ")
+  .map((str) => Number(str));
+
+let numArr = [];
+
+for (let i = 0; i < N; i++) {}
+
+console.log(N, M);
