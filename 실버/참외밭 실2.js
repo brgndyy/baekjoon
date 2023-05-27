8ee665c84c2720e8700bb13f11acaf35bd1b8c3d
brgndy <brgndy96@gmail.com>
2023-05-27 09:55:03 +0900

    msg

--- 실버/참외밭 실2.js
+++ 실버/참외밭 실2.js
@@ -1,7 +1,29 @@
-const input = `7
-4 50
-2 160
-3 30
-1 60
-3 20
-1 100`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let K = Number(input.shift());
+let directions = input.map((arr) => [
+  Number(arr.split(" ")[0]),
+  Number(arr.split(" ")[1]),
+]);
+let eOrW = [];
+let sOrN = [];
+directions.map((arr) => {
+  if (arr[0] === 1 || arr[0] === 2) {
+    eOrW.push(arr);
+  } else if (arr[0] === 3 || arr[0] === 4) {
+    sOrN.push(arr);
+  }
+});
+
+let eOrWMax = Math.max(...eOrW.map((arr) => arr[1]));
+let sOrNMax = Math.max(...sOrN.map((arr) => arr[1]));
+let eOrWMin = Math.min(...eOrW.map((arr) => arr[1]));
+let sOrNMin = Math.min(...sOrN.map((arr) => arr[1]));
+
+let totalArea = eOrWMax * sOrNMax;
+let minusArea = eOrWMin * sOrNMin;
+
+let resultArea = totalArea - minusArea;
+
+console.log(resultArea * K);
