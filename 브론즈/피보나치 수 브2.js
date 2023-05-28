26b1b7f0e9b42b165f7e916a4e4800c91e89fb7a
brgndy <brgndy96@gmail.com>
2023-05-28 12:11:40 +0900

    msg

--- 브론즈/피보나치 수 브2.js
+++ 브론즈/피보나치 수 브2.js
@@ -2,20 +2,4 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim();
 let N = Number(input);
 
-function fibonaci(n, memo = []) {
-  if (memo[n] !== undefined) {
-    return memo[n];
-  }
-
-  if (n <= 2) {
-    return 1;
-  }
-
-  let res = fibonaci(n - 1, memo) + fibonaci(n - 2, memo);
-
-  memo[n] = res;
-
-  return res;
-}
-
 console.log(fibonaci(N));
