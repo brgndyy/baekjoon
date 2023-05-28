4e5fe739aa66a166df04503c99a46ed4d06bbb8a
brgndy <brgndy96@gmail.com>
2023-05-28 17:25:20 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,32 +1,98 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = `11 12 2 24 10
+16 1 13 3 25
+6 20 5 21 17
+19 4 8 14 9
+22 15 7 23 18
+5 10 7 16 2
+4 22 8 17 13
+3 18 1 6 25
+12 19 23 14 21
+11 24 9 20 15`.split("\n");
 
-let arr = input.map((arr) => arr.split(" ").map((str) => Number(str)));
+let bingoArr = [];
+let numArr = [];
+let checkCount = 0;
 
-let totalArr = Array.from({ length: 100 }, () => Array(100).fill(0));
+for (let i = 0; i < 5; i++) {
+  let arr = input
+    .shift()
+    .split(" ")
+    .map((str) => Number(str));
+  bingoArr.push(arr);
+}
 
-for (let i = 0; i < arr.length; i++) {
-  // [1, 2]
+for (let j = 0; j < 5; j++) {
+  let arr = input
+    .shift()
+    .split(" ")
+    .map((str) => Number(str));
+  numArr.push(arr);
+}
 
-  let [oneX, oneY] = [arr[i][0], arr[i][1]];
-  // [4, 4]
-  let [twoX, twoY] = [arr[i][2], arr[i][3]];
+for (let i = 0; i < numArr.length; i++) {
+  for (let j = 0; j < numArr[i].length; j++) {
+    let callNum = numArr[i][j];
 
-  for (let j = oneX; j < twoX; j++) {
-    for (let k = oneY; k < twoY; k++) {
-      totalArr[k][j] += 1;
+    for (let k = 0; k < bingoArr.length; k++) {
+      for (let z = 0; z < bingoArr[k].length; z++) {
+        if (bingoArr[k][z] === callNum) {
+          checkCount++;
+          let answerCount = 0;
+          bingoArr[k][z] = 0;
+        }
+        for (let m = 0; m < 5; m++) {
+          let count = 0;
+          for (let n = 0; n < 5; n++) {
+            if (bingoArr[m][n] === 0) {
+              count++;
+            }
+          }
+          if (count === 5) {
+            answerCount++;
+          }
+        }
+
+        for (let a = 0; a < 5; a++) {
+          let count = 0;
+          for (let b = 0; b < 5; b++) {
+            if (bingoArr[b][a] === 0) {
+              count++;
+            }
+          }
+
+          if (count === 5) {
+            answerCount++;
+          }
+        }
+
+        for (let c = 0; c < 5; c++) {
+          let count = 0;
+
+          if (bingoArr[c][c] === 0) {
+            count++;
+          }
+
+          if (count === 5) {
+            answerCount++;
+          }
+        }
+
+        for (let d = 4; d >= 0; d--) {
+          let count = 0;
+          if (bingoArr[d][4 - d] === 0) {
+            count++;
+          }
+
+          if (count === 5) {
+            answerCount++;
+          }
+        }
+
+        if (answerCount === 3) {
+          console.log(checkCount);
+          return;
+        }
+      }
     }
   }
 }
-
-let count = 0;
-
-for (let i = 0; i < totalArr.length; i++) {
-  for (let j = 0; j < totalArr[i].length; j++) {
-    if (totalArr[i][j] >= 1) {
-      count++;
-    }
-  }
-}
-
-console.log(count);
