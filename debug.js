4d6b253b045b2f4eafacf7fa6148c47be6a1f294
brgndy <brgndy96@gmail.com>
2023-05-30 00:22:10 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,9 +1,8 @@
-const input = `5 4 7
+const input = `4 4 2
 1 2 3 4
-7 8 9 10
-13 14 15 16
-19 20 21 22
-25 26 27 28`.split("\n");
+5 6 7 8
+9 10 11 12
+13 14 15 16`.split("\n");
 
 let [N, M, R] = input
   .shift()
