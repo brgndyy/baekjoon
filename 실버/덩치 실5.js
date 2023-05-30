3df561f31a5120e1548d51d1e5c566a17dedd8d7
brgndy <brgndy96@gmail.com>
2023-05-31 08:40:17 +0900

    msg

--- 실버/덩치 실5.js
+++ 실버/덩치 실5.js
@@ -1,8 +1,26 @@
-const input = `5
-55 185
-58 183
-88 186
-60 175
-46 155`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let N = Number(input.shift());
+
+let arr = input.map(arr => arr.split(' ').map(str => Number(str)));
+let answer =[];
+
+for(let i = 0; i < arr.length; i++){
+    let [weight, height] = arr[i];
+    let rank = 1;
+    
+    for(let j = 0; j < arr.length; j++){
+        
+        if(i === j){
+            continue;
+        } else if(weight < arr[j][0] && height < arr[j][1]){
+            rank++;
+        }
+    }
+    
+    answer.push(rank);
+}
+
+
+console.log(answer.join(' '));
