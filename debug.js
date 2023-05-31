d8dcd8a17f3ac83240c9408eea0adc1f519078d4
brgndy <brgndy96@gmail.com>
2023-05-31 22:08:32 +0900

    Msg

--- debug.js
+++ debug.js
@@ -1,66 +1,42 @@
-const input = `4
-PPPP
-CYZY
-CCPY
-PPCC`.split("\n");
+const input = `11 10
+9785409507
+2055103694
+0861396761
+3073207669
+1233049493
+2300248968
+9769239548
+7984130001
+1670020095
+8894239889
+4053971072`.split("\n");
 
-let N = Number(input.shift());
-let board = input.map((arr) => arr.split(""));
+let [N, M] = input
+  .shift()
+  .split(" ")
+  .map((str) => Number(str));
+let board = input.map((arr) => arr.split("").map((str) => Number(str)));
 
-function swap(i, j, k) {
-  const coord = [
-    [-1, 0],
-    [1, 0],
-    [0, -1],
-    [0, 1],
-  ];
-  const here = board[i][j];
-  if (
-    board[i + coord[k][0]] &&
-    board[i + coord[k][0]][j + coord[k][1]] &&
-    here !== board[i + coord[k][0]][j + coord[k][1]]
-  ) {
-    board[i][j] = board[i + coord[k][0]][j + coord[k][1]];
-    board[i + coord[k][0]][j + coord[k][1]] = here;
-    return true;
-  } else return false;
-}
+let maxWidth = [1];
 
-function search() {
-  for (let l = 0; l < 2; l++) {
-    for (let x = 0; x < N; x++) {
-      let count = 0;
-      let color = curCandy(x, 0, l);
-      for (let y = 0; y < N; y++) {
-        if (curCandy(x, y, l) === color) {
-          count++;
-          if (count > maxCount) {
-            maxCount = count;
-          }
-        } else {
-          color = curCandy(x, y, l);
-          count = 1;
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    let point = board[i][j];
+
+    for (let z = j + 1; z < M; z++) {
+      if (point === board[i][z]) {
+        let minusNum = z - j;
+
+        if (
+          i + minusNum < N &&
+          point === board[i + minusNum][j] &&
+          point === board[i + minusNum][z]
+        ) {
+          maxWidth.push((minusNum + 1) * (minusNum + 1));
         }
       }
     }
   }
 }
 
-function curCandy(x, y, l) {
-  if (l === 0) return board[x][y];
-  else return board[y][x];
-}
-
-let maxCount = 0;
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    for (let k = 0; k < 4; k++) {
-      if (swap(i, j, k)) {
-        search();
-        swap(i, j, k);
-      }
-    }
-  }
-}
-
-console.log(maxCount);
+console.log(maxWidth);
