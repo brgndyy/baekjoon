615d9d91a9211a40cd5b14fcfe07c040748d08ee
brgndy <brgndy96@gmail.com>
2023-06-01 08:23:31 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,15 +1,5 @@
-const input = `11 10
-9785409507
-2055103694
-0861396761
-3073207669
-1233049493
-2300248968
-9769239548
-7984130001
-1670020095
-8894239889
-4053971072`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let [N, M] = input
   .shift()
@@ -39,4 +29,4 @@ for (let i = 0; i < N; i++) {
   }
 }
 
-console.log(maxWidth);
+console.log(Math.max(...maxWidth));
