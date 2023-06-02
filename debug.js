d9741abd4c38fe4f50df43c63150c11c448286ac
brgndy <brgndy96@gmail.com>
2023-06-02 13:06:59 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,4 +1,4 @@
-const input = `6 7 4
+const input = `6 7 2
 .......
 ...O...
 ....O..
@@ -11,77 +11,62 @@ let [R, C, N] = input
   .split(" ")
   .map((str) => Number(str));
 
-// map은 현재 보드의 상태
-// bombtime 은 각 칸에 있는 폭탄이 언제 터질지 나타내는 배열
+let board = input.map((line) => line.split(""));
 
-const map = Array.from({ length: R }, () => Array(C).fill(null));
-const bombtime = Array.from({ length: R }, () => Array(C).fill(0));
-// 북, 남, 서, 동
-const dir = [
+let bombTime = Array.from({ length: R }, () => Array(C).fill(0));
+
+let dir = [
   [-1, 0],
   [1, 0],
   [0, -1],
   [0, 1],
 ];
 
-let time = 1;
-
 for (let i = 0; i < R; i++) {
-  const temp = input[i].split("");
   for (let j = 0; j < C; j++) {
-    map[i][j] = temp[j];
-    if (temp[j] === "O") {
-      bombtime[i][j] = 3;
+    if (board[i][j] === "O") {
+      bombTime[i][j] = 3;
     }
   }
 }
+
+let time = 1;
 
 while (time <= N) {
   if (time % 2 === 0) {
     for (let i = 0; i < R; i++) {
       for (let j = 0; j < C; j++) {
-        if (map[i][j] === ".") {
-          map[i][j] = "O";
-          bombtime[i][j] = time + 3;
+        if (board[i][j] === ".") {
+          board[i][j] = "O";
+          bombTime[i][j] = time + 3;
         }
       }
     }
   } else {
     for (let i = 0; i < R; i++) {
       for (let j = 0; j < C; j++) {
-        if (bombtime[i][j] === time) {
-          map[i][j] = ".";
+        if (bombTime[i][j] === time) {
+          board[i][j] = ".";
+        }
 
-          for (let k = 0; k < 4; k++) {
-            const nx = i + dir[k][0];
-            const ny = j + dir[k][1];
+        for (let k = 0; k < 4; k++) {
+          let nx = i + dir[k][0];
+          let ny = j + dir[k][1];
 
-            if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+          if (nx < 0 || ny < 0 || nx >= R || ny >= C) {
+            continue;
+          }
 
-            if (map[nx][ny] === "O" && bombtime[nx][ny] !== time) {
-              map[nx][ny] = ".";
-              bombtime[nx][ny] = 0;
-            }
+          if (board[nx][ny] === "O" && bombTime[nx][ny] !== time) {
+            board[nx][ny] = ".";
+            bombTime[nx][ny] = 0;
           }
         }
       }
     }
   }
 
-  time += 1;
+  time++;
 }
 
-let answer = "";
-for (let i = 0; i < R; i++) {
-  answer += map[i].join("");
-  answer += "\n";
-}
-
-console.log(answer.trim());
-
-
-1. input 의 복사본 배열인 map 배열과 폭탄 설치 상태를 담은 bombTime 배열을 만들어준다.
-2. 시간을 1로 지정해놓고 만약에 N초가 되지 않은 상태라면 while 문을 실행한다.
-3. 그 안에서도 시간이 홀수초라면 (폭탄이 폭발되는 시간이라면) bombTime 배열에서 폭탄 폭발 되는 시간과 같다면 '.'으로 만들어준다.
-4. 또한 동서남북 방향으로 동서 남북 방향에서도 
-4. 시간이 짝수초라면 (폭탄을 설치하는 시간이라면)
+console.log(board);
--- 실버/봄버맨 실1.js
+++ 실버/봄버맨 실1.js
@@ -1,4 +1,4 @@
-const input = `6 7 4
+const input = `6 7 2
 .......
 ...O...
 ....O..
@@ -6,33 +6,22 @@ const input = `6 7 4
 OO.....
 OO.....`.split("\n");
 
-let [R, C, N] = input
-  .shift()
-  .split(" ")
-  .map((str) => Number(str));
-
-// map은 현재 보드의 상태
-// bombtime 은 각 칸에 있는 폭탄이 언제 터질지 나타내는 배열
-
+const [R, C, N] = input[0].split(" ").map((el) => +el);
 const map = Array.from({ length: R }, () => Array(C).fill(null));
 const bombtime = Array.from({ length: R }, () => Array(C).fill(0));
-// 북, 남, 서, 동
 const dir = [
   [-1, 0],
   [1, 0],
   [0, -1],
   [0, 1],
 ];
-
 let time = 1;
 
-for (let i = 0; i < R; i++) {
+for (let i = 1; i <= R; i++) {
   const temp = input[i].split("");
   for (let j = 0; j < C; j++) {
-    map[i][j] = temp[j];
-    if (temp[j] === "O") {
-      bombtime[i][j] = 3;
-    }
+    map[i - 1][j] = temp[j];
+    if (temp[j] === "O") bombtime[i - 1][j] = 3;
   }
 }
 
@@ -77,4 +66,4 @@ for (let i = 0; i < R; i++) {
   answer += "\n";
 }
 
-console.log(answer.trim());
+console.log(answer);
