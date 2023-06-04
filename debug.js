e51471a5958723a54d762377c41631bc0fb82c3e
brgndy <brgndy96@gmail.com>
2023-06-04 12:12:04 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,33 +1,71 @@
-const input = `2
-5 30 25 76 23 78
-6 25 50 70 99 70 90`.split("\n");
+const input = `7 6
+87`.split("\n");
 
-let answer = [];
+let [C, R] = input
+  .shift()
+  .split(" ")
+  .map((str) => Number(str));
 let K = Number(input.shift());
+
+let board = Array.from({ length: R + 1 }, () => Array(C + 1).fill(0));
+
+let dir = 4;
+let maxNum = C * R;
 let count = 1;
+let startPoint = 1;
+let heightNum = 1;
+let widthNum = 1;
 
-let sortedNums = input
-  .map((arr) => arr.split(" ").map((str) => Number(str)))
-  .map((arr) => arr.sort((a, b) => b - a));
-
-for (let i = 1; i <= sortedNums.length; i++) {
-  let classNum = i;
-  answer.push(classNum);
-  let maxNum = sortedNums[i - 1][0];
-  let minNum = sortedNums[i - 1][sortedNums[i - 1].length - 1];
-  let gap = 0;
-
-  for (let j = 0; j < sortedNums[i].length; j++) {
-    let minus = sortedNums[i][j] - sortedNums[i][j + 1];
-
-    if (minus > gap) {
-      gap = minus;
+if (maxNum < K) {
+  console.log(0);
+} else {
+  while (count <= K) {
+    if (dir === 4) {
+      for (let i = heightNum; i <= R && board[i][widthNum] === 0; i++) {
+        board[i][widthNum] = count++;
+        heightNum = i;
+      }
+      dir = 3;
+      widthNum++;
+    } else if (dir === 3) {
+      for (let i = widthNum; i <= C && board[heightNum][i] === 0; i++) {
+        board[heightNum][i] = count++;
+        widthNum = i;
+      }
+      dir = 2;
+      heightNum--;
+    } else if (dir === 2) {
+      for (
+        let i = heightNum;
+        i >= startPoint && board[i][widthNum] === 0;
+        i--
+      ) {
+        board[i][widthNum] = count++;
+        heightNum = i;
+      }
+      dir = 1;
+      widthNum--;
+    } else if (dir === 1) {
+      for (
+        let i = widthNum;
+        i >= startPoint && board[heightNum][i] === 0;
+        i--
+      ) {
+        board[heightNum][i] = count++;
+        widthNum = i;
+      }
+      dir = 4;
+      heightNum++;
+      startPoint++;
     }
   }
 
-  let str = `Max ${maxNum}, Min ${minNum}, Largest gap ${gap}`;
-
-  answer.push(str);
+  for (let i = 1; i <= R; i++) {
+    for (let j = 1; j <= C; j++) {
+      if (board[i][j] === K) {
+        console.log(j, i);
+        break;
+      }
+    }
+  }
 }
-
-console.log(answer);
