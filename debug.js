371c71e5676aacf9fcfd602fe98251eaa27aebc4
brgndy <brgndy96@gmail.com>
2023-06-06 11:19:30 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,33 +1,22 @@
-const input = `5 1
-3
-1
-5
-3`.split("\n");
+const input = `mobitel`;
 
-let [N, M] = input
-  .shift()
-  .split(" ")
-  .map((str) => Number(str));
-let j = Number(input.shift());
-let basketStartIdx = 0; // 바구니의 시작 위치
-let count = 0;
+let word = input;
 
-for (let i = 0; i < j; i++) {
-  let appleDropIdx = Number(input[i]) - 1; // 사과가 떨어지는 위치
+let minWord = input; // Start with the original word
 
-  // 사과가 바구니의 범위 내에 떨어지지 않는 경우
-  if (appleDropIdx < basketStartIdx || appleDropIdx >= basketStartIdx + M) {
-    // 사과가 바구니의 왼쪽 범위 밖에 떨어지는 경우
-    if (appleDropIdx < basketStartIdx) {
-      count += basketStartIdx - appleDropIdx;
-      basketStartIdx = appleDropIdx; // 바구니의 위치 업데이트
-    }
-    // 사과가 바구니의 오른쪽 범위 밖에 떨어지는 경우
-    else if (appleDropIdx >= basketStartIdx + M) {
-      count += appleDropIdx - (basketStartIdx + M - 1);
-      basketStartIdx = appleDropIdx - M + 1; // 바구니의 위치 업데이트
+// Iterate through all possible splits of the word into three parts
+for (let i = 1; i < word.length - 1; i++) {
+  for (let j = i + 1; j < word.length; j++) {
+    let part1 = word.slice(0, i).split("").reverse().join("");
+    let part2 = word.slice(i, j).split("").reverse().join("");
+    let part3 = word.slice(j).split("").reverse().join("");
+
+    let newWord = part1 + part2 + part3;
+
+    if (newWord < minWord) {
+      minWord = newWord; // Update if we found a word that is lexicographically smaller
     }
   }
 }
 
-console.log(count);
+console.log(minWord);
