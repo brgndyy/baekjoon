8eba4b4281a6e5750f9092a1c47a9bc7c3c2dee8
brgndy <brgndy96@gmail.com>
2023-06-07 13:43:40 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,36 +1,27 @@
-const input = `6 8
-.c......
-........
-.ccc..c.
-....c...
-..c.cc..
-....c...`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-let [H, W] = input
+let [K, L] = input
   .shift()
   .split(" ")
   .map((str) => Number(str));
 
-let board = input.map((arr) => arr.split(""));
 let answer = [];
+let studentMap = new Map();
 
-for (let i = 0; i < H; i++) {
-  let lineArr = [];
-  let count = -1;
-
-  for (let j = 0; j < W; j++) {
-    if (count === -1 && board[i][j] === ".") {
-      lineArr.push(count);
-    } else if (count >= 0 && board[i][j] === ".") {
-      count++;
-      lineArr.push(count);
-    } else if (board[i][j] === "c") {
-      count = 0;
-      lineArr.push(count);
-    }
+for (let i = 0; i < L; i++) {
+  if (studentMap.has(input[i])) {
+    studentMap.delete(input[i]);
+    studentMap.set(input[i], 1);
+  } else {
+    studentMap.set(input[i], 1);
   }
+}
 
-  answer.push(lineArr.join(" "));
+let studentArr = [...studentMap];
+
+for (let i = 0; i < K; i++) {
+  answer.push(studentArr[i][0]);
 }
 
 console.log(answer.join("\n"));
