d12bf21c2a0d9b14a899a519184e9e4ba1d50e34
brgndy <brgndy96@gmail.com>
2023-06-08 11:17:14 +0900

    msg

--- 실버/아시아 정보올림피아드 실5.js
+++ 실버/아시아 정보올림피아드 실5.js
@@ -1,6 +1,6 @@
 const input = `9
 1 1 230
-1 2 210
+2 2 210
 1 3 205
 2 1 100
 2 2 150
@@ -22,13 +22,9 @@ for (let i = 0; i < N; i++) {
   if (countryCnt[arr[i][0]] === undefined) {
     countryCnt[arr[i][0]] = 1;
     medal.push([arr[i][0], arr[i][1]]);
-  } else {
-    if (countryCnt[arr[i][0]] === 2) {
-      continue;
-    } else {
-      countryCnt[arr[i][0]] += 1;
-      medal.push([arr[i][0], arr[i][1]]);
-    }
+  } else if (countryCnt[arr[i][0]] < 2) {
+    countryCnt[arr[i][0]] += 1;
+    medal.push([arr[i][0], arr[i][1]]);
   }
 
   if (medal.length === 3) {
@@ -36,4 +32,6 @@ for (let i = 0; i < N; i++) {
   }
 }
 
-console.log(medal);
+let answer = medal.map((str) => str.join(" ")).join("\n");
+
+console.log(answer);
