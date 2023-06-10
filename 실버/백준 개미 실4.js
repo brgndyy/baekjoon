c7a2fd1564dab81a426e3281849988a1d50e0b78
brgndy <brgndy96@gmail.com>
2023-06-10 13:53:51 +0900

    msg

--- 실버/백준 개미 실4.js
+++ 실버/백준 개미 실4.js
@@ -1,7 +1,7 @@
-const input = `3 4
-JLA
-CRUO
-3`.split("\n");
+const input = `3 3
+ABC
+DEF
+2`.split("\n");
 
 let [N1, N2] = input[0].split(" ").map(Number);
 let right = input[1];
@@ -10,15 +10,14 @@ let T = Number(input[3]);
 
 right = right.split("").reverse().join("");
 
-let ant = right + left; // ALJCRUO
+let ant = right + left;
 let time = 0;
 
 while (time < T) {
-  // right => ALJ
-  // left => CRUO
   for (let i = 0; i < N1 + N2 - 1; i++) {
-    // i 가 2일때 right와 left 조건 둘다 충족
     if (right.includes(ant[i]) && left.includes(ant[i + 1])) {
+      // Swap ant[i] and ant[i+1]
+      let temp = ant[i];
       ant = ant.substring(0, i) + ant[i + 1] + ant[i] + ant.substring(i + 2);
       i++;
     }
