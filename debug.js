fdd7bd2da5034218f997844053a3641c8209af5c
brgndy <brgndy96@gmail.com>
2023-06-20 21:50:12 +0900

    msg

--- debug.js
+++ debug.js
@@ -1,48 +1,159 @@
-const input = `5 8
-100 99 60 80 30 20 10 89 99 100`.split("\n");
+const input = `3 3 0 0 16
+0 1 2
+3 4 5
+6 7 8
+4 4 1 1 3 3 2 2 4 4 1 1 3 3 2 2`.split("\n");
 
-let [N, K] = input.shift().split(" ").map(Number);
-let arr = input.shift().split(" ").map(Number);
-let stage = 0;
+function solution(input) {
+  let [N, M, x, y, K] = input
+    .shift()
+    .split(" ")
+    .map((str) => Number(str));
+  let board = [];
+  let startPos = [x, y];
+  // 맨윗면, 동쪽, 남쪽, 서쪽, 북쪽, 맨밑면
+  let dice = [0, 0, 0, 0, 0, 0];
+  let answer = [];
 
-function solution(N, K, arr) {
-  let robots = Array(arr.length).fill(false);
-  let count = 0;
+  let dir = {
+    1: [0, 1],
+    2: [0, -1],
+    3: [-1, 0],
+    4: [1, 0],
+  };
 
-  while (count < K) {
-    // Step 1
-    stage++;
-    arr.unshift(arr.pop());
-    robots.unshift(robots.pop());
-    // 내리는 위치에 도달한 로봇을 없애기
-    if (robots[N - 1]) {
-      robots[N - 1] = false;
+  for (let i = 0; i < N; i++) {
+    let arr = input
+      .shift()
+      .split(" ")
+      .map((str) => Number(str));
+    board.push(arr);
+  }
+
+  let orderArr = input
+    .shift()
+    .split(" ")
+    .map((str) => Number(str));
+
+  ///--------
+
+  for (let i = 0; i < K; i++) {
+    if (orderArr[i] === 1) {
+      let nx = startPos[0] + dir[1][0];
+      let ny = startPos[1] + dir[1][1];
+
+      // 범위 벗어나면 무시
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+        continue;
+      } else {
+        let boardNum = board[nx][ny];
+
+        tmp = dice[0];
+        dice[0] = dice[3];
+        dice[3] = dice[5];
+        dice[5] = dice[1];
+        dice[1] = tmp;
+
+        if (boardNum === 0) {
+          board[nx][ny] = dice[5];
+          dice[5] = 0;
+        } else if (boardNum !== 0) {
+          dice[5] = boardNum;
+          board[nx][ny] = 0;
+        }
+
+        startPos = [nx, ny];
+      }
+      answer.push(dice[0]);
     }
+    // 서쪽일때
+    else if (orderArr[i] === 2) {
+      let nx = startPos[0] + dir[2][0];
+      let ny = startPos[1] + dir[2][1];
 
-    // Step 2
-    for (let i = N - 1; i > 0; i--) {
-      if (!robots[i] && robots[i - 1] && arr[i] > 0) {
-        robots[i - 1] = false;
-        arr[i]--;
-        if (arr[i] === 0) {
-          count++;
+      // 범위 벗어나면 무시
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+        continue;
+      } else {
+        let boardNum = board[nx][ny];
+
+        tmp = dice[0];
+        dice[0] = dice[1];
+        dice[1] = dice[5];
+        dice[5] = dice[3];
+        dice[3] = tmp;
+
+        if (boardNum === 0) {
+          board[nx][ny] = dice[5];
+          dice[5] = 0;
+        } else if (boardNum !== 0) {
+          dice[5] = boardNum;
+          board[nx][ny] = 0;
         }
-        if (i !== N - 1) {
-          robots[i] = true;
-        }
+
+        startPos = [nx, ny];
+        answer.push(dice[0]);
       }
     }
+    // 북쪽일때
+    else if (orderArr[i] === 3) {
+      let nx = startPos[0] + dir[3][0];
+      let ny = startPos[1] + dir[3][1];
 
-    // Step 3
-    if (arr[0] !== 0) {
-      arr[0]--;
-      if (arr[0] === 0) {
-        count++;
+      // 범위 벗어나면 무시
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+        continue;
+      } else {
+        let boardNum = board[nx][ny];
+
+        tmp = dice[0];
+        dice[0] = dice[4];
+        dice[4] = dice[5];
+        dice[5] = dice[2];
+        dice[2] = tmp;
+
+        if (boardNum === 0) {
+          board[nx][ny] = dice[5];
+          dice[5] = 0;
+        } else if (boardNum !== 0) {
+          dice[5] = boardNum;
+          board[nx][ny] = 0;
+        }
+
+        startPos = [nx, ny];
+        answer.push(dice[0]);
       }
-      robots[0] = true;
+    } else if (orderArr[i] === 4) {
+      let nx = startPos[0] + dir[4][0];
+      let ny = startPos[1] + dir[4][1];
+
+      // 범위 벗어나면 무시
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+        continue;
+      } else {
+        let boardNum = board[nx][ny];
+
+        let tmp = dice[0];
+        dice[0] = dice[2];
+        dice[2] = dice[5];
+        dice[5] = dice[4];
+        dice[4] = tmp;
+
+        if (boardNum === 0) {
+          board[nx][ny] = dice[5];
+          dice[5] = 0;
+        } else if (boardNum !== 0) {
+          dice[5] = boardNum;
+          board[nx][ny] = 0;
+        }
+
+        startPos = [nx, ny];
+        answer.push(dice[0]);
+      }
     }
   }
-  return stage;
+
+  return answer.join("\n");
 }
 
-console.log(solution(N, K, arr));
+console.log(solution(input));
--- 골드/주사위 굴리기 골4.js
+++ 골드/주사위 굴리기 골4.js
@@ -1,46 +1,101 @@
-const input = `4 2 0 0 8
+const input = `4 2 0 0 3
 0 2
 3 4
 5 6
 7 8
-4 4 4 1 3 3 3 2`.split("\n");
+4 4 4`.split("\n");
 
-let [N, M, x, y, K] = input
-  .shift()
-  .split(" ")
-  .map((str) => Number(str));
+function solution(input) {
+  let [N, M, x, y, K] = input
+    .shift()
+    .split(" ")
+    .map((str) => Number(str));
+  let board = [];
+  let startPos = [x, y];
+  // 맨윗면, 동쪽, 남쪽, 서쪽, 북쪽, 맨밑면
+  let dice = [0, 0, 0, 0, 0, 0];
+  let answer = [];
 
-let moveDirArr = input
-  .pop()
-  .split(" ")
-  .map((str) => Number(str));
+  let dir = {
+    1: [0, 1],
+    2: [0, -1],
+    3: [-1, 0],
+    4: [1, 0],
+  };
 
-let map = input.map((str) => str.split(" ").map((str) => Number(str)));
-
-let dicePos = [x, y];
-// 맨 윗면, 맨 아랫면, 동, 서, 남, 북 방향
-let dice = [0, 0, 0, 0, 0, 0];
-let dirArr = [
-  [-1, 0],
-  [0, 1],
-  [1, 0],
-  [0, -1],
-];
-
-let answer = [];
-
-for (let i = 0; i < moveDirArr.length; i++) {
-  // 남쪽, 방향이 남쪽일땐 dice의 [4]가 움직임
-  if (moveDirArr[i] === 4) {
+  for (let i = 0; i < N; i++) {
+    let arr = input
+      .shift()
+      .split(" ")
+      .map((str) => Number(str));
+    board.push(arr);
   }
 
-  // 북쪽
-  else if (moveDirArr[i] === 3) {
+  let orderArr = input
+    .shift()
+    .split(" ")
+    .map((str) => Number(str));
+
+  ///--------
+
+  for (let i = 0; i < K; i++) {
+    // 동쪽일때
+    if (orderArr[i] === 1) {
+      let nx = startPos[0] + dir[1][0];
+      let ny = (startPos[1] = dir[1][1]);
+
+      // 범위 벗어나면 무시
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+        continue;
+      } else {
+        startPos = [nx, ny];
+      }
+    }
+    // 서쪽일때
+    else if (orderArr[i] === 2) {
+    }
+    // 북쪽일때
+    else if (orderArr[i] === 3) {
+    }
+    // 남쪽일때
+    // 맨윗면 => 남쪽
+    // 맨밑면 => 북쪽
+    // 남쪽 => 맨밑면
+    // 북쪽 => 맨윗면
+    // 동쪽 => 동쪽
+    // 서쪽 => 서쪽
+    else if (orderArr[i] === 4) {
+      let nx = startPos[0] + dir[4][0];
+      let ny = (startPos[1] = dir[4][1]);
+
+      // 범위 벗어나면 무시
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+        continue;
+      } else {
+        let boardNum = board[nx][ny];
+
+        let tmp = dice[0];
+        dice[0] = dice[2];
+        dice[2] = dice[5];
+        dice[5] = dice[4];
+        dice[4] = tmp;
+
+        if (boardNum === 0) {
+          board[nx][ny] = dice[5];
+          dice[5] = 0;
+        } else if (boardNum !== 0) {
+          dice[5] = boardNum;
+          board[nx][ny] = 0;
+        }
+
+        startPos = [nx, ny];
+      }
+
+      answer.push(dice[0]);
+    }
   }
-  // 서쪽
-  else if (moveDirArr[i] === 2) {
-  }
-  // 동쪽
-  else if (moveDirArr[i] === 1) {
-  }
+
+  return answer;
 }
+
+console.log(solution(input));
