5359988e4ac29c6294870c003b2d2990dfebf446
brgndy <brgndy96@gmail.com>
2024-01-28 17:24:38 +0900

    2805번 나무자르기

--- debug.js
+++ debug.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const [K, N] = input.shift().split(" ").map(Number);
+const [N, M] = input.shift().split(" ").map(Number);
 
-const arr = input.map(Number).sort((a, b) => a - b);
+const arr = input[0]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
 
 let start = 1;
 
@@ -17,13 +20,20 @@ while (start <= end) {
   let sum = 0;
 
   for (let x of arr) {
-    sum += Math.floor(x / mid);
+    if (x > mid) {
+      sum += x - mid;
+    }
   }
 
-  // 만든 랜선의 합이 더 짧다면
-  if (N > sum) {
+  // 만들어진 나무의 길이 합이 목표보다 짧다면,
+  // 즉 나무 길이를 더 짧게 해주어서 늘려주어야한다
+  if (M > sum) {
     end = mid - 1;
-  } else {
+  }
+
+  // 만들어진 나무 길이 합이 목표값보다 많다면
+  // 나무 길이를 더 늘려주어서 sum이 작아지도록 해야한다.
+  else {
     answer = mid;
     start = mid + 1;
   }
--- 실버/2805번 나무 자르기.js
+++ 실버/2805번 나무 자르기.js
@@ -1,21 +1,21 @@
-const input = `5 20
-4 42 40 26 46`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const [N, target] = input.shift().split(" ").map(Number);
+const [N, M] = input.shift().split(" ").map(Number);
 
 const arr = input[0]
   .split(" ")
   .map(Number)
   .sort((a, b) => a - b);
 
-let start = arr[0];
-
-let answer = 0;
+let start = 1;
 
 let end = arr[arr.length - 1];
 
+let answer = 0;
+
 while (start <= end) {
-  let mid = Math.floor((start + end) / 2);
+  const mid = Math.floor((start + end) / 2);
 
   let sum = 0;
 
@@ -25,12 +25,17 @@ while (start <= end) {
     }
   }
 
-  // 나무를 더 많이 잘랐다면
-  if (sum >= target) {
-    start = mid + 1;
-    answer = mid;
-  } else {
+  // 만들어진 나무의 길이 합이 목표보다 짧다면,
+  // 즉 나무 길이를 더 짧게 해주어서 늘려주어야한다
+  if (M > sum) {
     end = mid - 1;
+  }
+
+  // 만들어진 나무 길이 합이 목표값보다 많다면
+  // 나무 길이를 더 늘려주어서 sum이 작아지도록 해야한다.
+  else {
+    answer = mid;
+    start = mid + 1;
   }
 }
 
