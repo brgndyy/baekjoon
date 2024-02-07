7eaa0e0be6dfeda5a97456d83b3fbd4d37d31187
brgndy <brgndy96@gmail.com>
2024-02-08 06:06:03 +0900

    10971번 외판원 순회 2

--- 실버/10971번 외판원 순회2.js
+++ 실버/10971번 외판원 순회2.js
@@ -1,8 +1,5 @@
-const input = `4
-0 10 15 20
-5 0 9 10
-6 13 0 12
-8 8 9 0`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const N = Number(input.shift());
 
@@ -11,6 +8,10 @@ const cityArr = input.map((city) => city.split(" ").map(Number));
 const visited = Array(N).fill(false);
 
 let min = Number.MAX_SAFE_INTEGER;
+
+// start => 현재 외판원이 방문하고 있는 도시
+
+// cost => 현재까지 외판원이 방문하는데에 든 비용
 
 function dfs(depth, start, cost) {
   if (depth === N - 1 && cityArr[start][0] !== 0) {
@@ -34,7 +35,3 @@ visited[0] = true;
 dfs(0, 0, 0);
 
 console.log(min);
-
-// start => 현재 위치하고 있는 도시
-
-// cost => 현재까지의 비용
