46ed8f7267e07bc12a91b66a6b49557d7c3637cf
brgndy <brgndy96@gmail.com>
2024-02-15 08:19:23 +0900

    2529번 부등호(틀림)

--- debug.js
+++ debug.js
@@ -1,41 +1,12 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = `9
+> < < < > > > < <`.split("\n");
 
-let [R, C] = input.shift().split(" ").map(Number);
+let K = Number(input.shift());
 
-const mapArr = input.map((str) => str.split(""));
+let inqualitySignArr = input[0].split(" ");
 
-let dx = [1, 0, -1, 0];
-let dy = [0, 1, 0, -1];
+let numArr = Array.from({ length: 10 }, (_, index) => index);
 
-let visited = new Set();
+let answerArr = [];
 
-let maxCount = 1;
-
-function dfs(depth, x, y) {
-  maxCount = Math.max(maxCount, depth);
-
-  for (let i = 0; i < 4; i++) {
-    let nx = x + dx[i];
-
-    let ny = y + dy[i];
-
-    if (
-      nx >= 0 &&
-      ny >= 0 &&
-      nx < R &&
-      ny < C &&
-      !visited.has(mapArr[nx][ny])
-    ) {
-      visited.add(mapArr[nx][ny]);
-      dfs(depth + 1, nx, ny);
-      visited.delete(mapArr[nx][ny]);
-    }
-  }
-}
-
-visited.add(mapArr[0][0]);
-
-dfs(1, 0, 0);
-
-console.log(maxCount);
+function dfs(depth) {}
---
+++ 실버/2529반 부등호.js
@@ -0,0 +1,8 @@
+const input = `9
+> < < < > > > < <`.split("\n");
+
+let K = Number(input.shift());
+
+let inqualitySignArr = input.split(" ");
+
+console.log(inqualitySignArr);
