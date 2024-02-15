92d7a837fa3ccd33c958beae2bcd28aef94c03ce
brgndy <brgndy96@gmail.com>
2024-02-16 07:52:40 +0900

    2529번 부등호

--- debug.js
+++ debug.js
@@ -1,12 +1,61 @@
 const input = `9
 > < < < > > > < <`.split("\n");
 
-let K = Number(input.shift());
+const k = Number(input.shift());
 
-let inqualitySignArr = input[0].split(" ");
+const inequalitySignArr = input[0].split(" ");
 
-let numArr = Array.from({ length: 10 }, (_, index) => index);
+let visited = Array(10).fill(false);
 
 let answerArr = [];
 
-function dfs(depth) {}
+let result = [];
+
+let current = "";
+
+let first = "";
+
+function dfs(depth) {
+  if (depth === k + 1) {
+    let check = true;
+
+    for (let i = 0; i < k; i++) {
+      if (inequalitySignArr[i] === "<") {
+        if (result[i] > result[i + 1]) {
+          check = false;
+        }
+      } else if (inequalitySignArr[i] === ">") {
+        if (result[i] < result[i + 1]) {
+          check = false;
+        }
+      }
+    }
+
+    if (check) {
+      current = "";
+
+      for (let x of result) {
+        current += x + "";
+      }
+
+      if (first === "") {
+        first = current;
+      }
+    }
+  }
+
+  for (let i = 0; i < 10; i++) {
+    if (visited[i]) {
+      continue;
+    }
+    visited[i] = true;
+    result.push(i);
+    dfs(depth + 1);
+    result.pop();
+    visited[i] = false;
+  }
+}
+
+dfs(0);
+
+console.log(current + "\n" + first);
--- 실버/2529반 부등호.js
+++ 실버/2529반 부등호.js
@@ -1,8 +1,61 @@
 const input = `9
 > < < < > > > < <`.split("\n");
 
-let K = Number(input.shift());
+const k = Number(input.shift());
 
-let inqualitySignArr = input.split(" ");
+const inequalitySignArr = input[0].split(" ");
 
-console.log(inqualitySignArr);
+let visited = Array(10).fill(false);
+
+let answerArr = [];
+
+let result = [];
+
+let current = "";
+
+let first = "";
+
+function dfs(depth) {
+  if (depth === k + 1) {
+    let check = true;
+
+    for (let i = 0; i < k; i++) {
+      if (inequalitySignArr[i] === "<") {
+        if (result[i] > result[i + 1]) {
+          check = false;
+        }
+      } else if (inequalitySignArr[i] === ">") {
+        if (result[i] < result[i + 1]) {
+          check = false;
+        }
+      }
+    }
+
+    if (check) {
+      current = "";
+
+      for (let x of result) {
+        current += x + "";
+      }
+
+      if (first === "") {
+        first = current;
+      }
+    }
+  }
+
+  for (let i = 0; i < 10; i++) {
+    if (visited[i]) {
+      continue;
+    }
+    visited[i] = true;
+    result.push(i);
+    dfs(depth + 1);
+    result.pop();
+    visited[i] = false;
+  }
+}
+
+dfs(0);
+
+console.log(current + "\n" + first);
