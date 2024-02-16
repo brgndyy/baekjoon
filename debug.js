bc431f51aaad50060f79a14905b1798def38a6d0
brgndy <brgndy96@gmail.com>
2024-02-16 09:25:15 +0900

    2606 바이러스(틀림)

--- debug.js
+++ debug.js
@@ -1,61 +1,16 @@
-const input = `9
-> < < < > > > < <`.split("\n");
+const input = `7
+6
+1 2
+2 3
+1 5
+5 2
+5 6
+4 7`.split("\n");
 
-const k = Number(input.shift());
+const computerNumber = Number(input.shift());
 
-const inequalitySignArr = input[0].split(" ");
+const computerPairNumber = Number(input.shift());
 
-let visited = Array(10).fill(false);
+const computerArr = input.map((computer) => computer.split(" ").map(Number));
 
-let answerArr = [];
-
-let result = [];
-
-let current = "";
-
-let first = "";
-
-function dfs(depth) {
-  if (depth === k + 1) {
-    let check = true;
-
-    for (let i = 0; i < k; i++) {
-      if (inequalitySignArr[i] === "<") {
-        if (result[i] > result[i + 1]) {
-          check = false;
-        }
-      } else if (inequalitySignArr[i] === ">") {
-        if (result[i] < result[i + 1]) {
-          check = false;
-        }
-      }
-    }
-
-    if (check) {
-      current = "";
-
-      for (let x of result) {
-        current += x + "";
-      }
-
-      if (first === "") {
-        first = current;
-      }
-    }
-  }
-
-  for (let i = 0; i < 10; i++) {
-    if (visited[i]) {
-      continue;
-    }
-    visited[i] = true;
-    result.push(i);
-    dfs(depth + 1);
-    result.pop();
-    visited[i] = false;
-  }
-}
-
-dfs(0);
-
-console.log(current + "\n" + first);
+let visited = Array(computerPairNumber).fill(false);
