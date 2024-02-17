18404acdd268e28cf21e6ad3528443d37dfbb264
brgndy <brgndy96@gmail.com>
2024-02-17 10:00:46 +0900

    dfs

--- debug.js
+++ debug.js
@@ -7,10 +7,5 @@ const input = `7
 5 6
 4 7`.split("\n");
 
-const computerNumber = Number(input.shift());
-
-const computerPairNumber = Number(input.shift());
-
-const computerArr = input.map((computer) => computer.split(" ").map(Number));
-
-let visited = Array(computerPairNumber).fill(false);
+const n = Number(input[0]); // 정점의 개수
+const m = Number(input[1]); // 간선의 개수
