515153999cbc2f8b07c1fd4652faa282cc584082
brgndy <brgndy96@gmail.com>
2024-02-22 07:24:53 +0900

    2606번 바이러스

--- debug.js
+++ debug.js
@@ -1,50 +1,33 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-let T = Number(input.shift());
+const vertexNumber = Number(input.shift());
 
-let dx = [1, -1, 0, 0];
-let dy = [0, 0, 1, -1];
+const edgesNumber = Number(input.shift());
 
-while (T--) {
-  let [M, N, K] = input.shift().split(" ").map(Number);
-  let field = Array.from({ length: N }, () => Array(M).fill(0));
-  let visited = Array.from({ length: N }, () => Array(M).fill(false));
-  let worms = 0;
+let graph = Array.from({ length: vertexNumber + 1 }, () => []);
 
-  for (let i = 0; i < K; i++) {
-    let [x, y] = input.shift().split(" ").map(Number);
-    field[y][x] = 1;
-  }
+let visited = Array(vertexNumber).fill(false);
 
-  function dfs(x, y) {
-    visited[x][y] = true;
+let answer = 0;
 
-    for (let dir = 0; dir < 4; dir++) {
-      let nx = x + dx[dir];
-      let ny = y + dy[dir];
-
-      if (
-        nx >= 0 &&
-        nx < N &&
-        ny >= 0 &&
-        ny < M &&
-        !visited[nx][ny] &&
-        field[nx][ny] === 1
-      ) {
-        dfs(nx, ny);
-      }
-    }
-  }
-
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      if (field[i][j] === 1 && !visited[i][j]) {
-        dfs(i, j);
-        worms++;
-      }
-    }
-  }
-
-  console.log(worms);
+for (let i = 0; i < edgesNumber; i++) {
+  let [x, y] = input[i].split(" ").map(Number);
+  graph[y].push(x);
+  graph[x].push(y);
 }
+
+function dfs(vertex) {
+  visited[vertex] = true;
+  answer += 1;
+
+  for (let x of graph[vertex]) {
+    if (!visited[x]) {
+      dfs(x);
+    }
+  }
+}
+
+dfs(1);
+
+console.log(answer - 1);
--- 실버/2606번 바이러스.js
+++ 실버/2606번 바이러스.js
@@ -1,36 +1,28 @@
-const input = `7
-6
-1 2
-2 3
-1 5
-5 2
-5 6
-4 7`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-let N = Number(input.shift());
-let M = Number(input.shift());
+const vertexNumber = Number(input.shift());
 
-// let graph = Array(N + 1).fill([]); // 이러면 모두 같은 주소값을 갖는 배열이 되어버린다.
-let graph = Array.from({ length: N + 1 }, () => []);
+const edgesNumber = Number(input.shift());
 
-let visited = Array(N + 1).fill(0);
+let graph = Array.from({ length: vertexNumber + 1 }, () => []);
+
+let visited = Array(vertexNumber).fill(false);
 
 let answer = 0;
 
-for (let i = 0; i < M; i++) {
+for (let i = 0; i < edgesNumber; i++) {
   let [x, y] = input[i].split(" ").map(Number);
-  graph[x].push(y);
   graph[y].push(x);
+  graph[x].push(y);
 }
 
 function dfs(vertex) {
   visited[vertex] = true;
-  answer++;
+  answer += 1;
 
   for (let x of graph[vertex]) {
-    // 더 깊이 탐색하기 위해 반복문
     if (!visited[x]) {
-      // 방문처리가 되어있지 않다면
       dfs(x);
     }
   }
@@ -38,4 +30,4 @@ function dfs(vertex) {
 
 dfs(1);
 
-console.log(answer - 1); // 1번 노드는 제외해야하기때문에 -1을 해준다.
+console.log(answer - 1);
