3183a6246a73057d7b2c45865dabd4c6501444f4
brgndy <brgndy96@gmail.com>
2024-02-22 08:31:18 +0900

    1240번 (틀림)

--- debug.js
+++ debug.js
@@ -1,33 +1,38 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = `4 2
+2 1 2
+4 3 2
+1 4 3
+1 2
+3 2`.split("\n");
 
-const vertexNumber = Number(input.shift());
+const [N, M] = input.shift().split(" ").map(Number);
 
-const edgesNumber = Number(input.shift());
+let graph = Array.from({ length: N + 1 }, () => []);
 
-let graph = Array.from({ length: vertexNumber + 1 }, () => []);
 
-let visited = Array(vertexNumber).fill(false);
+// 각 좌표의 비용을 담은 graph 배열 생성
+for (let i = 0; i < N - 1; i++) {
+  const [x, y, cost] = input.shift().split(" ").map(Number);
 
-let answer = 0;
-
-for (let i = 0; i < edgesNumber; i++) {
-  let [x, y] = input[i].split(" ").map(Number);
-  graph[y].push(x);
-  graph[x].push(y);
+  graph[x].push([y, cost]);
+  graph[y].push([x, cost]);
 }
 
-function dfs(vertex) {
-  visited[vertex] = true;
-  answer += 1;
+for(let i = 0; i < M; i++){
+    let [x, y] = input.shift().split(' ').map(Number);
 
-  for (let x of graph[vertex]) {
-    if (!visited[x]) {
-      dfs(x);
-    }
-  }
+    let visited = Array(N+1).fill(false);
+    let distance = Array(N+1).fill(-1);
+
+    dfs(x, 0, visited, distance);
+
+
 }
 
-dfs(1);
+function dfs(x, y, visited, totalCost = 0){
+    if(visis)
+}
 
-console.log(answer - 1);
+
+
+console.log(graph);
