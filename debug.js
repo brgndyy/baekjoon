bc8f06b5b31547cb97b3290b4e01d41203a4b8de
brgndy <brgndy96@gmail.com>
2024-03-03 21:21:17 +0900

    1697번 숨바꼭질 (틀림)

--- debug.js
+++ debug.js
@@ -1,55 +1,34 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [N, K] = `5 17`.split(" ").map(Number);
 
-const N = Number(input[0]);
+const MAX = 100000;
 
-let apartments = [];
+const visitedHistory = Array(MAX + 1).fill(0);
 
-let visited = Array.from({ length: N }, () => Array(N).fill(false));
+let answer = 1;
 
-let dx = [1, 0, -1, 0];
-let dy = [0, 1, 0, -1];
+let queue = [N];
 
-let answer = [];
+visitedHistory[N] = 1;
 
-for (let i = 1; i <= N; i++) {
-  const line = input[i].split("").map(Number);
+function bfs() {
+  while (queue.length) {
+    const targetNumber = queue.shift();
 
-  apartments.push(line);
-}
+    if (targetNumber === K) {
+      return visitedHistory[targetNumber] - 1;
+    }
 
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    if (apartments[i][j] === 1 && !visited[i][j]) {
-      answer.push(dfs(i, j));
+    for (let next of [targetNumber - 1, targetNumber + 1, targetNumber * 2]) {
+      if (next < 0 || next > MAX) {
+        continue;
+      }
+
+      if (visitedHistory[next] === 0) {
+        queue.push(next);
+        visitedHistory[next] = visitedHistory[targetNumber] + 1;
+      }
     }
   }
 }
 
-function dfs(x, y) {
-  visited[x][y] = true;
-
-  let count = 1;
-
-  for (let i = 0; i < 4; i++) {
-    let nx = x + dx[i];
-    let ny = y + dy[i];
-
-    if (
-      nx >= 0 &&
-      ny >= 0 &&
-      nx < N &&
-      ny < N &&
-      !visited[nx][ny] &&
-      apartments[nx][ny] === 1
-    ) {
-      count += dfs(nx, ny);
-    }
-  }
-
-  return count;
-}
-
-console.log(answer.length);
-answer.sort((a, b) => a - b);
-console.log(answer.join("\n"));
+console.log(bfs());
