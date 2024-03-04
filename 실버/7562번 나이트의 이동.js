e1becb81a1046659e4aedf51ca4cdc01ba9baf7a
brgndy <brgndy96@gmail.com>
2024-03-04 10:45:09 +0900

    백준 7562번 나이트의 이동

--- 실버/7562번 나이트의 이동.js
+++ 실버/7562번 나이트의 이동.js
@@ -1,43 +1,44 @@
-const input = `3
-8
-0 0
-7 0
-100
-0 0
-30 50
-10
-1 1
-1 1`.split("\n");
-
-const testCase = Number(input[0]);
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let dx = [1, 2, 2, 1, -1, -2, -2, -1];
 let dy = [-2, -1, 1, 2, 2, 1, -1, -2];
 
-let line = 1;
-
-for (let t = 0; t < testCase; t++) {
-  const chessBoardLength = Number(input[line++]);
-  const [currentX, currentY] = input[line++].split(" ").map(Number);
-  const [destinationX, destinationY] = input[line++].split(" ").map(Number);
+for (let i = 1; i < input.length; i++) {
+  let chessBoardLength = Number(input[i]);
 
   let visited = Array.from({ length: chessBoardLength }, () =>
     Array(chessBoardLength).fill(0)
   );
-  let queue = [[currentX, currentY, 0]];
-  visited[currentX][currentY] = 1; // 시작 위치 방문으로 표시
+
+  let [knightPositionRow, knightPositionCol] = input[i + 1]
+    .split(" ")
+    .map(Number);
+
+  let [destinantionPositionRow, destinantionPositionCol] = input[i + 2]
+    .split(" ")
+    .map(Number);
+
+  let queue = [];
+
+  queue.push([knightPositionRow, knightPositionCol]);
+
+  visited[knightPositionRow][knightPositionCol] = 1;
 
   function bfs() {
-    while (queue.length > 0) {
-      const [x, y, depth] = queue.shift();
+    while (queue.length) {
+      const [currentRow, currentCol] = queue.shift();
 
-      if (x === destinationX && y === destinationY) {
-        return depth; // 목적지에 도착한경우 depth 반환
+      if (
+        currentRow === destinantionPositionRow &&
+        currentCol === destinantionPositionCol
+      ) {
+        return visited[currentRow][currentCol] - 1;
       }
 
       for (let i = 0; i < 8; i++) {
-        const nx = x + dx[i];
-        const ny = y + dy[i];
+        let nx = currentRow + dx[i];
+        let ny = currentCol + dy[i];
 
         if (
           nx >= 0 &&
@@ -46,12 +47,15 @@ for (let t = 0; t < testCase; t++) {
           ny < chessBoardLength &&
           visited[nx][ny] === 0
         ) {
-          visited[nx][ny] = 1; // 방문 표시
-          queue.push([nx, ny, depth + 1]); // 더 넓은 범위로 탐색
+          queue.push([nx, ny]);
+
+          visited[nx][ny] = visited[currentRow][currentCol] + 1;
         }
       }
     }
   }
 
   console.log(bfs());
+
+  i += 2;
 }
