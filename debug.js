f3f613b7885b69b050149b750853f979bd4f9f37
brgndy <brgndy96@gmail.com>
2024-03-05 07:31:02 +0900

    1707번 (틀림)

--- debug.js
+++ debug.js
@@ -1,34 +1,60 @@
-const [N, K] = `5 17`.split(" ").map(Number);
+const input = `2
+3 2
+1 3
+2 3
+4 4
+1 2
+2 3
+3 4
+4 2`.split("\n");
 
-const MAX = 100000;
+function isBipartite(graph, vertex) {
+  const color = Array(vertex + 1).fill(0);
 
-const visitedHistory = Array(MAX + 1).fill(0);
+  function bfs(vertex) {
+    const queue = [];
+    queue.push(vertex);
+    color[vertex] = 1;
 
-let answer = 1;
+    while (queue.length) {
+      const targetVertex = queue.shift();
 
-let queue = [N];
-
-visitedHistory[N] = 1;
-
-function bfs() {
-  while (queue.length) {
-    const targetNumber = queue.shift();
-
-    if (targetNumber === K) {
-      return visitedHistory[targetNumber] - 1;
+      for (let next of graph[targetVertex]) {
+        if (color[next] === 0) {
+          // ?
+          color[next] = -color[targetVertex];
+          queue.push(next);
+        } else if (color[targetVertex] === color[next]) {
+          return false;
+        }
+      }
     }
 
-    for (let next of [targetNumber - 1, targetNumber + 1, targetNumber * 2]) {
-      if (next < 0 || next > MAX) {
-        continue;
-      }
+    return true;
+  }
 
-      if (visitedHistory[next] === 0) {
-        queue.push(next);
-        visitedHistory[next] = visitedHistory[targetNumber] + 1;
-      }
+  for (let i = 1; i <= vertex; i++) {
+    if (color[i] === 0 && !bfs(i)) {
+      return false;
     }
   }
+
+  return true;
 }
 
-console.log(bfs());
+for (let i = 1; i < input.length; i++) {
+  const [vertex, edgeNumber] = input[i].split(" ").map(Number);
+
+  const graph = Array.from({ length: vertex + 1 }, () => []);
+
+  for (let j = i + 1; j <= i + edgeNumber; j++) {
+    const [x, y] = input[j].split(" ").map(Number);
+
+    graph[x].push(y);
+    graph[y].push(x);
+  }
+
+  console.log(isBipartite(graph, vertex) ? "YES" : "NO");
+
+  i += edgeNumber;
+}
