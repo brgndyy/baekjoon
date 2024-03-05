a7f9930ecc303e4541a06cea16cd95eb3c5c39b1
brgndy <brgndy96@gmail.com>
2024-03-06 07:30:42 +0900

    1707번 이분그래프

--- debug.js
+++ debug.js
@@ -8,53 +8,53 @@ const input = `2
 3 4
 4 2`.split("\n");
 
-function isBipartite(graph, vertex) {
-  const color = Array(vertex + 1).fill(0);
+const testCase = Number(input[0]);
 
-  function bfs(vertex) {
-    const queue = [];
-    queue.push(vertex);
-    color[vertex] = 1;
+for (let i = 1; i < input.length; i++) {
+  const [V, E] = input[i].split(" ").map(Number);
+  const graph = Array.from({ length: V + 1 }, () => []);
 
-    while (queue.length) {
-      const targetVertex = queue.shift();
+  function isBipartiteGraph(graph, V) {
+    const color = Array(V + 1).fill(0);
 
-      for (let next of graph[targetVertex]) {
-        if (color[next] === 0) {
-          // ?
-          color[next] = -color[targetVertex];
-          queue.push(next);
-        } else if (color[targetVertex] === color[next]) {
-          return false;
+    function bfs(vertex) {
+      const queue = [];
+      queue.push(vertex);
+      color[vertex] = 1;
+
+      while (queue.length) {
+        const targetVertex = queue.shift();
+
+        for (let next of graph[targetVertex]) {
+          if (color[next] === 0) {
+            color[next] = -color[targetVertex];
+            queue.push(next);
+          }
+          if (color[next] === color[targetVertex]) {
+            return false;
+          }
         }
+      }
+
+      return true;
+    }
+
+    for (let i = 1; i <= V; i++) {
+      if (color[i] === 0 && !bfs(i)) {
+        return false;
       }
     }
 
     return true;
   }
 
-  for (let i = 1; i <= vertex; i++) {
-    if (color[i] === 0 && !bfs(i)) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-for (let i = 1; i < input.length; i++) {
-  const [vertex, edgeNumber] = input[i].split(" ").map(Number);
-
-  const graph = Array.from({ length: vertex + 1 }, () => []);
-
-  for (let j = i + 1; j <= i + edgeNumber; j++) {
+  for (let j = i + 1; j <= i + E; j++) {
     const [x, y] = input[j].split(" ").map(Number);
-
     graph[x].push(y);
     graph[y].push(x);
   }
 
-  console.log(isBipartite(graph, vertex) ? "YES" : "NO");
+  console.log(isBipartiteGraph(graph, V) ? "YES" : "NO");
 
-  i += edgeNumber;
+  i += E;
 }
--- 골드/1707번 이분 그래프.js
+++ 골드/1707번 이분 그래프.js
@@ -8,50 +8,53 @@ const input = `2
 3 4
 4 2`.split("\n");
 
-function isBipartiteBfs(graph, vertex) {
-  let color = Array(vertex + 1).fill(0);
+const testCase = Number(input[0]);
 
-  function bfs(vertex) {
-    const queue = [];
-    queue.push(vertex);
-    color[vertex] = 1;
+for (let i = 1; i < input.length; i++) {
+  const [V, E] = input[i].split(" ").map(Number);
+  const graph = Array.from({ length: V + 1 }, () => []);
 
-    while (queue.length) {
-      const targetVertex = queue.shift();
+  function isBipartiteGraph(graph, V) {
+    const color = Array(V + 1).fill(0);
 
-      for (let next of graph[targetVertex]) {
-        if (color[next] === 0) {
-          color[next] = -color[targetVertex];
-          queue.push(next);
-        } else if (color[next] === color[targetVertex]) {
-          return false;
+    function bfs(vertex) {
+      const queue = [];
+      queue.push(vertex);
+      color[vertex] = 1;
+
+      while (queue.length) {
+        const targetVertex = queue.shift();
+
+        for (let next of graph[targetVertex]) {
+          if (color[next] === 0) {
+            color[next] = -color[targetVertex];
+            queue.push(next);
+          }
+          if (color[next] === color[targetVertex]) {
+            return false;
+          }
         }
+      }
+
+      return true;
+    }
+
+    for (let i = 1; i <= V; i++) {
+      if (color[i] === 0 && !bfs(i)) {
+        return false;
       }
     }
 
     return true;
   }
 
-  for (let i = 1; i <= vertex; i++) {
-    if (color[i] === 0 && !bfs(i)) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-for (let i = 1; i < input.length; i++) {
-  const [V, E] = input[i].split(" ").map(Number);
-
-  let graph = Array.from({ length: V + 1 }, () => []);
-
   for (let j = i + 1; j <= i + E; j++) {
-    let [x, y] = input[j].split(" ").map(Number);
-
+    const [x, y] = input[j].split(" ").map(Number);
     graph[x].push(y);
     graph[y].push(x);
   }
-  console.log(isBipartiteBfs(graph, V) ? "YES" : "NO");
+
+  console.log(isBipartiteGraph(graph, V) ? "YES" : "NO");
+
   i += E;
 }
