69dfc0741c16b52de7052d103501da2dcda7c88c
brgndy <brgndy96@gmail.com>
2024-03-08 07:51:33 +0900

    2583번 영역 구하기 (틀림)

---
+++ 실버/2583번 영역 구하기.js
@@ -0,0 +1,21 @@
+const input = `5 7 3
+0 2 4 4
+1 1 2 5
+4 0 6 2`.split("\n");
+
+const [M, N, K] = input[0].split(" ").map(Number);
+const paper = Array.from({ length: M }, () => Array(N).fill(0));
+const rectangles = input.slice(1).map((line) => line.split(" ").map(Number));
+
+// x1, x2 같은 경우 직사각형의 가로 범위
+// y1과 y2 같은 경우 직사각형의 세로 범위
+
+rectangles.forEach(([x1, y1, x2, y2]) => {
+  for (let i = M - y2; i < M - y1; i++) {
+    for (let j = x1; j < x2; j++) {
+      paper[i][j] = 1;
+    }
+  }
+});
+
+console.log(paper);
