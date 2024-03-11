ba4b6d743f37041dfcf31f004ad73a9e43ef4381
brgndy <brgndy96@gmail.com>
2024-03-12 06:27:37 +0900

    섬의 개수

--- debug.js
+++ debug.js
@@ -31,23 +31,20 @@ for (let i = 0; i < input.length; i++) {
   if (i === input.length - 1) {
     break;
   }
-
   const [w, h] = input[i].split(" ").map(Number);
 
-  const graph = [];
-
-  for (let j = 1 + i; j <= i + h; j++) {
-    const line = input[j].split(" ").map(Number);
-
-    graph.push(line);
-  }
-
+  const field = [];
   let visited = Array.from({ length: h }, () => Array(w).fill(0));
   let count = 0;
 
+  for (let j = i + 1; j <= i + h; j++) {
+    const line = input[j].split(" ").map(Number);
+    field.push(line);
+  }
+
   for (let i = 0; i < h; i++) {
     for (let j = 0; j < w; j++) {
-      if (graph[i][j] === 1 && visited[i][j] === 0) {
+      if (field[i][j] && visited[i][j] === 0) {
         bfs(i, j);
         count++;
       }
@@ -59,20 +56,20 @@ for (let i = 0; i < input.length; i++) {
     visited[i][j] = 1;
 
     while (queue.length) {
-      const [x, y] = queue.shift();
-      visited[x][y] = true;
+      const [currentX, currentY] = queue.shift();
+      visited[currentX][currentY] = 1;
 
       for (let i = 0; i < 8; i++) {
-        let nx = dx[i] + x;
-        let ny = dy[i] + y;
+        const nx = currentX + dx[i];
+        const ny = currentY + dy[i];
 
         if (
           nx >= 0 &&
           ny >= 0 &&
           nx < h &&
           ny < w &&
-          graph[nx][ny] === 1 &&
-          visited[nx][ny] === 0
+          visited[nx][ny] === 0 &&
+          field[nx][ny] === 1
         ) {
           queue.push([nx, ny]);
         }
