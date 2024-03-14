7638cca3241a0ecfffaefa121176c6b06500ed6f
brgndy <brgndy96@gmail.com>
2024-03-15 06:06:45 +0900

    2583번 영역구하기

--- 실버/2583번 영역 구하기.js
+++ 실버/2583번 영역 구하기.js
@@ -7,6 +7,8 @@ const [M, N, K] = input[0].split(" ").map(Number);
 const paper = Array.from({ length: M }, () => Array(N).fill(0));
 const rectangles = input.slice(1).map((line) => line.split(" ").map(Number));
 
+// M은 모눈 종이의 세로줄
+// N은 모눈 종이의 가로줄
 // x1, x2 같은 경우 직사각형의 가로 범위
 // y1과 y2 같은 경우 직사각형의 세로 범위
 
