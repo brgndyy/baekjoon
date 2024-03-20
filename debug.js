6866f40a55e0b25bb59171c92cbffcfacafcaf96
brgndy <brgndy96@gmail.com>
2024-03-21 06:11:03 +0900

    1463번

--- debug.js
+++ debug.js
@@ -1,21 +1,35 @@
-const input = `3
-4
-7
-10`.split("\n");
+const input = `10`;
 
-const testCase = Number(input[0]);
+let N = Number(input);
 
-for (let i = 1; i < input.length; i++) {
-  let N = Number(input[i]);
+let dp = Array(N + 1).fill(0);
 
-  let dp = Array(N + 1).fill(0);
-  dp[0] = 1;
-  dp[1] = 1;
-  dp[2] = 2;
+for (let i = 2; i <= N; i++) {
+  dp[i] = dp[i - 1] + 1;
 
-  for (let i = 3; i <= N; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-  }
+  if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
 
-  console.log(dp[N]);
+  if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
 }
+
+console.log(dp[N]);
+// if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+
+// if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+
+// const input = `10`;
+
+// let N = Number(input);
+
+// const dp = new Array(N + 1).fill(0);
+// // dp[i]는 숫자 i를 1로 만드는데 필요한 최소 연산 횟수
+
+// for (let i = 2; i <= N; i++) {
+//   dp[i] = dp[i - 1] + 1;
+
+//   if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+
+//   if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+// }
+
+// console.log(dp[N]);
