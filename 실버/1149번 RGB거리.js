a2ab69d2fbebbce57896044e996e330aedb1df7a
brgndy <brgndy96@gmail.com>
2024-03-28 07:14:00 +0900

    1149번 RGB거리

---
+++ 실버/1149번 RGB거리.js
@@ -0,0 +1,29 @@
+const input = `3
+26 40 83
+49 60 57
+13 89 99`.split("\n");
+
+const N = Number(input[0]);
+
+let arr = [];
+
+for (let i = 1; i <= N; i++) {
+  const line = input[i].split(" ").map(Number);
+  arr.push(line);
+}
+
+let dp = Array.from({ length: N }, () => Array(3).fill(0));
+
+for (let j = 0; j < 3; j++) {
+  dp[0][j] = arr[0][j];
+}
+
+for (let i = 1; i < N; i++) {
+  dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0]; // 빨강
+  dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1]; // 초록
+  dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2]; // 파랑
+}
+
+const result = Math.min(...dp[N - 1]);
+
+console.log(result);
