0ba0c3795886bff34041ca9c20160988d19aeb15
brgndy <brgndy96@gmail.com>
2024-03-28 07:39:04 +0900

    1149 RGB 상세 풀이 추가

--- 실버/1149번 RGB거리.js
+++ 실버/1149번 RGB거리.js
@@ -14,16 +14,20 @@ for (let i = 1; i <= N; i++) {
 
 let dp = Array.from({ length: N }, () => Array(3).fill(0));
 
+// 1번째 집을 빨강, 초록, 파랑 순서대로 칠했을때의 비용을 담는다.
 for (let j = 0; j < 3; j++) {
   dp[0][j] = arr[0][j];
 }
 
 for (let i = 1; i < N; i++) {
-  dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0]; // 빨강
-  dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1]; // 초록
-  dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2]; // 파랑
+  // i번째 집에서 초록, 파랑 순서대로 칠했을때 비용 + 기존에 빨간색으로 칠한 비용을 누적시켜준다.
+  dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+
+  // i번째 집에서 빨강, 파랑 순서대로 칠했을때 비용 + 기존에 초록색으로 칠한 비용을 누적시켜준다.
+  dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+
+  // i번째 집에서 빨강, 초록 순서대로 칠했을때 비용 + 기존에 파랑색으로 칠한 비용을 누적시켜준다.
+  dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
 }
 
-const result = Math.min(...dp[N - 1]);
-
-console.log(result);
+console.log(Math.min(...dp[N - 1]));
