b8cb1414a2bdf4e7ce06590485bb3940f287909c
brgndy <brgndy96@gmail.com>
2024-04-01 07:57:20 +0900

    1149번 RGB거리

--- debug.js
+++ debug.js
@@ -1,23 +1,30 @@
-const fs = require("fs");
-const input = fs
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n")
-  .map(Number);
+const input = `3
+26 40 83
+49 60 57
+13 89 99`.split("\n");
 
-const T = Number(input[0]);
+const N = Number(input[0]);
 
-for (let i = 1; i <= T; i++) {
-  let zeroCount = 1;
-  let oneCount = 0;
+const arr = [];
 
-  for (let j = 1; j <= input[i]; j++) {
-    const tmpCount = zeroCount;
+for (let i = 1; i <= N; i++) {
+  const line = input[i].split(" ").map(Number);
 
-    zeroCount = oneCount;
-    oneCount += tmpCount;
-  }
-
-  console.log(zeroCount + " " + oneCount);
+  arr.push(line);
 }
+
+const dp = Array.from({ length: N }, () => Array(3).fill(0));
+
+for (let i = 0; i < N; i++) {
+  dp[0][i] = arr[0][i];
+}
+
+for (let i = 1; i < N; i++) {
+  dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+
+  dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+
+  dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+}
+
+console.log(Math.min(...dp[N - 1]));
