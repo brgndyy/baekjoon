19214bd355e5696dd5617593e56e24e95f431d47
brgndy <brgndy96@gmail.com>
2024-04-02 07:41:22 +0900

    11053번 가장 긴 증가하는 부분 수열

--- debug.js
+++ debug.js
@@ -1,30 +1,22 @@
-const input = `3
-26 40 83
-49 60 57
-13 89 99`.split("\n");
+const input = `6
+20 20 10 30 20 50`.split("\n");
 
 const N = Number(input[0]);
 
-const arr = [];
+const arr = input[1].split(" ").map(Number);
 
-for (let i = 1; i <= N; i++) {
-  const line = input[i].split(" ").map(Number);
-
-  arr.push(line);
-}
-
-const dp = Array.from({ length: N }, () => Array(3).fill(0));
+const dp = Array(N).fill(1);
 
 for (let i = 0; i < N; i++) {
-  dp[0][i] = arr[0][i];
+  for (let j = 0; j < i; j++) {
+    if (arr[j] < arr[i]) {
+      // 여기서 dp[i]는 `i`번째 요소까지 고려했을때 가장 긴 증가하는 수열을 저장
+      // dp[j]는 arr[j]를 마지막으로 하는 가장 긴 증가하는 부분 수열의 길이
+      // Math.max(dp[i], dp[j] + 1)의 의미
+      // 이 부분은 dp[i]의 현재 값(이전에 계산된 값)과 새로 계산된 값(dp[j] + 1) 중 더 큰 값을 dp[i]에 저장
+      dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+  }
 }
 
-for (let i = 1; i < N; i++) {
-  dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-
-  dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-
-  dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
-}
-
-console.log(Math.min(...dp[N - 1]));
+console.log(Math.max(...dp));
--- 실버/11053번 가장 긴 증가하는 부분 수열.js
+++ 실버/11053번 가장 긴 증가하는 부분 수열.js
@@ -2,6 +2,7 @@ const input = `6
 20 20 10 30 20 50`.split("\n");
 
 const N = Number(input[0]);
+
 const arr = input[1].split(" ").map(Number);
 
 const dp = Array(N).fill(1);
@@ -9,10 +10,13 @@ const dp = Array(N).fill(1);
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < i; j++) {
     if (arr[j] < arr[i]) {
+      // 여기서 dp[i]는 `i`번째 요소까지 고려했을때 가장 긴 증가하는 수열을 저장
+      // dp[j]는 arr[j]를 마지막으로 하는 가장 긴 증가하는 부분 수열의 길이
+      // Math.max(dp[i], dp[j] + 1)의 의미
+      // 이 부분은 dp[i]의 현재 값(이전에 계산된 값)과 새로 계산된 값(dp[j] + 1) 중 더 큰 값을 dp[i]에 저장
       dp[i] = Math.max(dp[i], dp[j] + 1);
     }
   }
 }
 
-const maxLIS = Math.max(...dp);
-console.log(maxLIS);
+console.log(Math.max(...dp));
