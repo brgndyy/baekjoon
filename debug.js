99ea322240306e068af6e7751c495b6ef7e05d87
brgndy <brgndy96@gmail.com>
2024-04-03 06:35:25 +0900

    2579계단오르기

--- debug.js
+++ debug.js
@@ -1,18 +1,25 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = `6
+10
+20
+15
+25
+10
+20`
+  .split("\n")
+  .map(Number);
 
 const N = Number(input[0]);
 
-const arr = input[1].split(" ").map(Number);
+const dp = Array(N + 1).fill(0);
 
-const dp = Array(N).fill(1);
+dp[1] = input[1];
 
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < i; j++) {
-    if (arr[j] < arr[i]) {
-      dp[i] = Math.max(dp[i], dp[j] + 1);
-    }
-  }
+if (N >= 2) {
+  dp[2] = input[1] + input[2];
 }
 
-console.log(Math.max(...dp));
+for (let i = 3; i <= N; i++) {
+  dp[i] = Math.max(dp[i - 2] + input[i], dp[i - 3] + input[i - 1] + input[i]);
+}
+
+console.log(dp);
