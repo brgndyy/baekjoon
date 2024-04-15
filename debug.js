8e105849324810eab85e3d5a83740d35afe1dc27
brgndy <brgndy96@gmail.com>
2024-04-16 07:45:50 +0900

    2579번 계단오르기

--- debug.js
+++ debug.js
@@ -8,18 +8,18 @@ const input = `6
   .split("\n")
   .map(Number);
 
-const N = Number(input[0]);
+const N = input[0];
 
 const dp = Array(N + 1).fill(0);
 
 dp[1] = input[1];
 
 if (N >= 2) {
-  dp[2] = input[1] + input[2];
+  dp[2] = dp[1] + input[2];
 }
 
 for (let i = 3; i <= N; i++) {
-  dp[i] = Math.max(dp[i - 2] + input[i], dp[i - 3] + input[i - 1] + input[i]);
+  dp[i] = Math.max(dp[i - 2] + input[i], dp[i - 3] + input[i - 2] + input[i]);
 }
 
 console.log(dp);
