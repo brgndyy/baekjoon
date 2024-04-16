d9a3ab1de0e0f736c3188cd5636490717687bf61
brgndy <brgndy96@gmail.com>
2024-04-17 06:10:19 +0900

    1912번 연속합

--- 실버/1912번 연속합.js
+++ 실버/1912번 연속합.js
@@ -1,17 +1,16 @@
-const input = `5
--1 -2 -3 -4 -5`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const N = Number(input.shift());
-const arr = input[0].split(" ").map(Number);
+const N = Number(input[0]);
+
+const arr = input[1].split(" ").map(Number);
+
 const dp = Array(N).fill(0);
 
 dp[0] = arr[0];
-let maxSum = dp[0];
 
 for (let i = 1; i < N; i++) {
   dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
-
-  maxSum = Math.max(maxSum, dp[i]);
 }
 
-console.log(maxSum);
+console.log(Math.max(...dp));
