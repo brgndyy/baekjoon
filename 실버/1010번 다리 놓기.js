d0988914efb072374e0b1923d665370ee81d6a1b
brgndy <brgndy96@gmail.com>
2024-04-17 07:53:11 +0900

    1010 다리놓기

--- 실버/1010번 다리 놓기.js
+++ 실버/1010번 다리 놓기.js
@@ -5,19 +5,16 @@ const input = `3
 
 const T = Number(input[0]);
 
-// DP 테이블 생성 및 초기화
 const max = 30;
 const dp = Array.from({ length: max + 1 }, () => Array(max + 1).fill(0));
 
-// 기본 조합 설정
+// 맨 왼쪽, 맨 오른쪽은 무조건 1로 초기화
 for (let i = 0; i <= max; i++) {
   dp[i][0] = 1;
   dp[i][i] = 1;
 }
 
-console.log(dp);
-
-// 조합 값 계산 (파스칼의 삼각형)
+// 가장 자리를 제외한 중간 부분을 계산해주기 (끝부분은 0으로 초기화가 됐기 때문에 j < i)
 for (let i = 2; i <= max; i++) {
   for (let j = 1; j < i; j++) {
     dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
@@ -30,3 +27,4 @@ for (let t = 1; t <= T; t++) {
   console.log(dp[M][N]);
 }
 // 여기서 dp[i][j]는 i개의 아이템 중에서 j개를 선택할수 있는 경우의 수
+// 그래서 M개 중에서 N개를 선택할수 있는 경우의 수를 골라야함
