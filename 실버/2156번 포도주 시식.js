02f7838b8daa4365bb06e06fe3948ae9bb3b8cc5
brgndy <brgndy96@gmail.com>
2024-05-04 12:47:29 +0900

    2156 포도주

--- 실버/2156번 포도주 시식.js
+++ 실버/2156번 포도주 시식.js
@@ -8,27 +8,22 @@ const input = `6
   .split("\n")
   .map(Number);
 
-const N = input.shift();
+const N = input[0];
 
-if (N === 0) {
-  console.log(0);
-  return;
-}
-
-const wines = input;
 const dp = Array(N + 1).fill(0);
 
-dp[1] = wines[0];
-if (N > 1) {
-  dp[2] = wines[0] + wines[1];
+dp[1] = input[1];
+
+if (N >= 2) {
+  dp[2] = input[1] + input[2];
 }
 
 for (let i = 3; i <= N; i++) {
   dp[i] = Math.max(
-    dp[i - 1], // 현재 잔을 마시지 않는 경우
-    dp[i - 2] + wines[i - 1], // 바로 이전 잔을 마시지 않고 현재 잔을 마시는 경우
-    dp[i - 3] + wines[i - 2] + wines[i - 1] // 연속된 두 잔을 마시고, 그 이전 잔은 마시지 않는 경우
+    dp[i - 1],
+    dp[i - 2] + input[i],
+    dp[i - 3] + input[i - 1] + input[i]
   );
 }
 
-console.log(dp[N]);
+console.log(dp);
