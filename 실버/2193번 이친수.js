fb153cca5d3196a71de9f4d9c63087c66f709875
brgndy <brgndy96@gmail.com>
2024-05-06 05:56:36 +0900

    2193 이친수

--- 실버/2193번 이친수.js
+++ 실버/2193번 이친수.js
@@ -1,4 +1,5 @@
-const input = `3`;
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const N = Number(input);
 
@@ -9,8 +10,8 @@ const dp = Array(MAX + 1).fill(0);
 dp[1] = 1;
 dp[2] = 1;
 
-for (let i = 3; i <= MAX; i++) {
+for (let i = 3; i <= N; i++) {
   dp[i] = BigInt(dp[i - 2]) + BigInt(dp[i - 1]);
 }
 
-console.log(dp[N]);
+console.log(dp[i]);
