e940a4783ca8bb2575b3fa744ba732d0d49ab0f7
brgndy <brgndy96@gmail.com>
2024-05-14 07:09:26 +0900

    토마토

--- debug.js
+++ debug.js
@@ -1,29 +1,80 @@
-const input = `6
-6
-10
-13
-9
-8
-1`
-  .split("\n")
-  .map(Number);
+const input = `5 3 2
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 1 0 0
+0 0 0 0 0`.split("\n");
 
-const N = Number(input[0]);
+const [N, M, H] = input.shift().split(" ").map(Number);
 
-const dp = Array(N + 1).fill(0);
+const boxes = Array.from({ length: H }, () =>
+  Array.from({ length: M }, () => Array(N).fill(0))
+);
 
-dp[1] = input[1];
+let queue = [];
 
-if (N >= 2) {
-  dp[2] = input[1] + input[2];
+const directions = [
+  [1, 0, 0], // 윗층
+  [-1, 0, 0], // 아랫층
+  [0, 1, 0], // 앞
+  [0, -1, 0], // 뒤
+  [0, 0, 1], // 오
+  [0, 0, -1], // 왼
+];
+
+let days = 0;
+
+for (let i = 0; i < H; i++) {
+  for (let j = 0; j < M; j++) {
+    boxes[i][j] = input.shift().split(" ").map(Number);
+
+    for (let k = 0; k < N; k++) {
+      if (boxes[i][j][k] === 1) {
+        queue.push([i, j, k, 0]);
+      }
+    }
+  }
 }
 
-for (let i = 3; i <= N; i++) {
-  dp[i] = Math.max(
-    dp[i],
-    dp[i - 2] + input[i],
-    dp[i - 3] + input[i - 1] + input[i]
-  );
+while (queue.length) {
+  const [df, dw, dh, day] = queue.shift();
+  days = day;
+
+  for (let i = 0; i < 6; i++) {
+    const nf = df + directions[i][0];
+    const nw = dw + directions[i][1];
+    const nh = dh + directions[i][2];
+
+    if (
+      nf >= 0 &&
+      nf < H &&
+      nw >= 0 &&
+      nw < M &&
+      nh >= 0 &&
+      nh < N &&
+      boxes[nf][nw][nh] === 0
+    ) {
+      boxes[nf][nw][nh] = 1;
+      queue.push([nf, nw, nh, day + 1]);
+    }
+  }
 }
 
-console.log(dp);
+let allRipe = true;
+for (let i = 0; i < H; i++) {
+  for (let j = 0; j < M; j++) {
+    for (let k = 0; k < N; k++) {
+      if (boxes[i][j][k] === 0) {
+        allRipe = false;
+        break;
+      }
+    }
+  }
+}
+
+if (allRipe) {
+  console.log(days);
+} else {
+  console.log(-1);
+}
